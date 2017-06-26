d105852c105960286c1cecce68969642623d2a51
david getta <david getta>
2017-06-26 08:56:14 +0700

    add model

--- application/models/Marker_model.php
+++ application/models/Marker_model.php
@@ -9,10 +9,8 @@ class Marker_model extends CI_Model {
 
         public function getAllDetail ($type=0){
 
-        	if($type !=){
-				$s_type=addslashes($type);
-				$this->db->where("(type LIKE '%".$s_type."%')");
-        	}
+		$s_type=addslashes($type);
+		$this->db->where("(type LIKE '%".$s_type."%')");
                 $query = $this->db->get('location');
                 return $query->result_array();
         }
